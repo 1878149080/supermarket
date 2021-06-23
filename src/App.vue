5e68e4a5498d92d19ad4ec790644613cafadf469
苟宏 <1878149080@qq.com>
2021-06-23 17:07:48 +0800

    bar完成

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
-    
+    <router-view></router-view>
+    <main-bar></main-bar>
   </div>
 </template>
 
 <script>
-
+import MainBar from './components/common/main-bar.vue'
 export default {
   name: 'App',
   components: {
+    MainBar
   }
 }
 </script>
 
 <style>
-
+@import "./assets/css/base.css";
+@import "./assets/font_8lnsx54cnu/iconfont.css";
 </style>
